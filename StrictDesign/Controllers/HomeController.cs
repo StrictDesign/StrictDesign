e95d9bc9d40cb1f8a09a9c315b6243387f2ebc92
Jimmy <Jimmy@CELTIC>
2017-10-24 21:47:10 +0200

    Add project files.

---
+++ StrictDesign/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StrictDesign.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.BackgroundColor = "#fff";
+            return View();
+        }
+        [Authorize]
+        public ActionResult Hemlig()
+        {
+            ViewBag.Message = "Detta är en hemlig sida.";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ StrictDesign/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StrictDesign.MvcApplication" Language="C#" %>
